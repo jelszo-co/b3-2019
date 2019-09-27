847b146b5c712e73702102804cd96b1dbaba6fe6
Tasztalos Pista <tasztalos69@gmail.com>
2019-09-27 20:28:27 +0200

    Complete form

--- Round2/Game.js
+++ Round2/Game.js
@@ -2,6 +2,8 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -18,18 +20,102 @@ var Game = function (_React$Component) {
 
 		var _this = _possibleConstructorReturn(this, (Game.__proto__ || Object.getPrototypeOf(Game)).call(this, props));
 
-		_this.state = { liked: false };
+		_this.state = {
+			gameStarted: false,
+			formRows: "",
+			formCols: "",
+			minValue: 5,
+			maxValue: 20
+		};
 		return _this;
 	}
 
 	_createClass(Game, [{
 		key: "render",
 		value: function render() {
-			return React.createElement(
-				"p",
-				null,
-				"Ap\xE1dat"
-			);
+			var _this2 = this;
+
+			var _state = this.state,
+			    gameStarted = _state.gameStarted,
+			    formRows = _state.formRows,
+			    formCols = _state.formCols,
+			    minValue = _state.minValue,
+			    maxValue = _state.maxValue;
+
+			var onSubmit = function onSubmit(e) {
+				e.preventDefault();
+				if (formRows < minValue || formRows > maxValue || formCols < minValue || formCols > maxValue) {
+					alert("Hopsz! Az \xE9rt\xE9keknek " + minValue + " \xE9s " + maxValue + " k\xF6z\xF6tt kell lenni\xFCk.");
+				} else {
+					_this2.setState({ gameStarted: true });
+				}
+				console.log("submitted");
+			};
+
+			var onChange = function onChange(e) {
+				_this2.setState(_defineProperty({}, e.target.name, e.target.value));
+			};
+			if (gameStarted) {
+				var table = [];
+				for (var i = 0; i < formRows; i++) {
+					table.push("asd ");
+				}
+				return React.createElement(
+					"div",
+					null,
+					table
+				);
+			} else {
+				return React.createElement(
+					"div",
+					null,
+					React.createElement(
+						"h2",
+						null,
+						"T\xE1bla gener\xE1l\xE1sa"
+					),
+					React.createElement(
+						"p",
+						null,
+						"Minimum ",
+						React.createElement(
+							"b",
+							null,
+							minValue + "x" + minValue
+						),
+						", maximum",
+						" ",
+						React.createElement(
+							"b",
+							null,
+							maxValue + "x" + maxValue
+						),
+						" n\xE9gyzet"
+					),
+					React.createElement(
+						"form",
+						{ onSubmit: onSubmit },
+						React.createElement("input", {
+							type: "number",
+							name: "formRows",
+							placeholder: "Sorok",
+							value: formRows,
+							onChange: onChange,
+							autoFocus: true,
+							required: true
+						}),
+						React.createElement("input", {
+							type: "number",
+							name: "formCols",
+							placeholder: "Oszlopok",
+							value: formCols,
+							onChange: onChange,
+							required: true
+						}),
+						React.createElement("input", { type: "submit", value: "Gener\xE1l\xE1s!" })
+					)
+				);
+			}
 		}
 	}]);
 
--- Round2/readable/Game.js
+++ Round2/readable/Game.js
@@ -5,11 +5,74 @@ const e = React.createElement;
 class Game extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { liked: false };
+		this.state = {
+			gameStarted: false,
+			formRows: "",
+			formCols: "",
+			minValue: 5,
+			maxValue: 20
+		};
 	}
 
 	render() {
-		return <p>Apádat</p>;
+		const { gameStarted, formRows, formCols, minValue, maxValue } = this.state;
+		const onSubmit = (e) => {
+			e.preventDefault();
+			if (
+				formRows < minValue ||
+				formRows > maxValue ||
+				formCols < minValue ||
+				formCols > maxValue
+			) {
+				alert(
+					`Hopsz! Az értékeknek ${minValue} és ${maxValue} között kell lenniük.`
+				);
+			} else {
+				this.setState({ gameStarted: true });
+			}
+			console.log("submitted");
+		};
+
+		const onChange = (e) => {
+			this.setState({ [e.target.name]: e.target.value });
+		};
+		if (gameStarted) {
+			let table = [];
+			for (let i = 0; i < formRows; i++) {
+				table.push("asd ");
+			}
+			return <div>{table}</div>;
+		} else {
+			return (
+				<div>
+					<h2>Tábla generálása</h2>
+					<p>
+						Minimum <b>{minValue + "x" + minValue}</b>, maximum{" "}
+						<b>{maxValue + "x" + maxValue}</b> négyzet
+					</p>
+					<form onSubmit={onSubmit}>
+						<input
+							type="number"
+							name="formRows"
+							placeholder="Sorok"
+							value={formRows}
+							onChange={onChange}
+							autoFocus={true}
+							required
+						/>
+						<input
+							type="number"
+							name="formCols"
+							placeholder="Oszlopok"
+							value={formCols}
+							onChange={onChange}
+							required
+						/>
+						<input type="submit" value="Generálás!" />
+					</form>
+				</div>
+			);
+		}
 	}
 }
 
